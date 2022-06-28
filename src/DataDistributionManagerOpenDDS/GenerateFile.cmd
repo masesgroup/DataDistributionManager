1927f14d152d3000cf3634568221445479eb7150
MASES Public Developers Team <94312179+masesdevelopers@users.noreply.github.com>
2022-06-28 03:08:45 +0200

    Update to Visual Studio 2022 (#39)
    * Upgrade to Visual Studio 2022
    
    * Revert JDK to version 11 from 13
    
    * Fix javac command line
    
    * Remove .NET 5 setup
    
    * Test update

--- src/DataDistributionManagerOpenDDS/GenerateFile.cmd
+++ src/DataDistributionManagerOpenDDS/GenerateFile.cmd
@@ -1,3 +1,3 @@
 @echo off
 perl %ACE_ROOT%\bin\generate_export_file.pl DataDistributionSchemaCommon > DataDistributionSchemaCommon_Export.h
-perl %ACE_ROOT%/bin/mwc.pl -type vs2019 DataDistributionManagerOpenDDS.mwc
+perl %ACE_ROOT%/bin/mwc.pl -type vs2022 DataDistributionManagerOpenDDS.mwc
